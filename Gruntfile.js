29126ff3fbc2c0175cc17055e9c9ddb2be254180
swapnil106111 <swap106111@gmail.com>
2019-11-29 18:03:07 +0530

    #0 | converted to new bundle format

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,55 @@
-const rulesConfigInfra = require('rules-config/infra');
-const IDI = require('openchs-idi');
+const rulesConfigInfra = require("rules-config/infra");
+const IDI = require("openchs-idi");
 
-module.exports = IDI.configure({
-    "name": "jnpct",
+module.exports = IDI.configure(
+  {
+    name: "jnpct",
     "chs-admin": "admin",
     "org-name": "JNPCT",
     "org-admin": "admin@jnpct",
-    "secrets": '../secrets.json',
-    "files": {
-        "adminUsers": {
-            "dev": ["users/admin-user.json"],
-        },
-        "forms": [
-            "registration/registrationForm.json",
-            "eligibleCouple/eligibleCoupleEnrolmentForm.json",
-            "eligibleCouple/eligibleCoupleFollowupForm.json",
-            "pregnancy/pregnancyEnrolmentForm.json",
-            "pregnancy/ancForm.json",
-            "pregnancy/deliveryForm.json",
-            "pregnancy/abortionForm.json",
-            "pregnancy/abortionFollowupForm.json",
-            "pregnancy/motherPNCForm.json",
-            "child/childEnrolment.json",
-            "child/childBirthForm.json",
-            "child/childPNCForm.json",
-            "child/childFollowupForm.json"
-        ],
-        "formMappings": [
-            "metadata/formMappings.json"
-        ],
-        "formDeletions": [],
-        "formAdditions": [],
-        "catchments": [
-            "metadata/catchments.json"
-        ],
-        "checklistDetails": [],
-        "concepts": [
-            "registration/registrationConcepts.json",
-            "eligibleCouple/eligibleCoupleConcepts.json",
-            "pregnancy/pregnancyEnrolmentConcepts.json",
-            "pregnancy/ancFormConcepts.json",
-            "child/childBirthConcepts.json",
-            "pregnancy/deliveryConcepts.json",
-            "pregnancy/abortionConcepts.json",
-            "child/childConcepts.json",
-            "child/childPNCConcepts.json",
-            "child/childFollowupConcepts.json"
-        ],
-        "locations": [
-            "metadata/locations.json"
-        ],
-        "programs": ["programs.json"],
-        "encounterTypes": [
-            "metadata/encounterTypes.json"
-        ],
-        "operationalEncounterTypes": [
-            "metadata/operationalEncounterTypes.json"
-        ],
-        "operationalPrograms": [
-            "metadata/operationalPrograms.json"
-        ],
-        "subjectTypes": ["subjectTypes.json"],
-        "operationalSubjectTypes": [
-            "metadata/operationalSubjectTypes.json"
-        ],
-        "users": {
-            "dev": ["users/dev-users.json"]
-        },
-        "rules": [
-            "./rules.js"
-        ],
-        "organisationSql": [
-            /* "create_organisation.sql"*/
-        ],
-        "organisationConfig": ["organisationConfig.json"],
-        "translations": [
-            "translations/en.json"
-
-        ]
-
+    secrets: "../secrets.json",
+    files: {
+      adminUsers: {
+        dev: ["users/admin-user.json"]
+      },
+      forms: [
+        "forms/registrationForm.json",
+        "forms/eligibleCoupleEnrolmentForm.json",
+        "forms/eligibleCoupleFollowupForm.json",
+        "forms/pregnancyEnrolmentForm.json",
+        "forms/ancForm.json",
+        "forms/deliveryForm.json",
+        "forms/abortionForm.json",
+        "forms/abortionFollowupForm.json",
+        "forms/motherPNCForm.json",
+        "forms/childEnrolment.json",
+        "forms/childBirthForm.json",
+        "forms/childPNCForm.json",
+        "forms/childFollowupForm.json"
+      ],
+      formMappings: ["metadata/formMappings.json"],
+      formDeletions: [],
+      formAdditions: [],
+      catchments: ["metadata/catchments.json"],
+      checklistDetails: [],
+      concepts: ["concepts.json"],
+      locations: ["metadata/locations.json"],
+      programs: ["programs.json"],
+      encounterTypes: ["metadata/encounterTypes.json"],
+      operationalEncounterTypes: ["metadata/operationalEncounterTypes.json"],
+      operationalPrograms: ["metadata/operationalPrograms.json"],
+      subjectTypes: ["subjectTypes.json"],
+      operationalSubjectTypes: ["metadata/operationalSubjectTypes.json"],
+      users: {
+        dev: ["users/dev-users.json"]
+      },
+      rules: ["./rules.js"],
+      organisationSql: [
+        /* "create_organisation.sql"*/
+      ],
+      organisationConfig: ["organisationConfig.json"],
+      translations: ["translations/en.json"]
     }
-}, rulesConfigInfra);
+  },
+  rulesConfigInfra
+);
