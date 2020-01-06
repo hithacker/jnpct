479998a4f91ed49f640c40874342b64fb9e6c10d
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2020-01-06 15:37:35 +0530

    #31 | UAT mother module changes

--- pregnancy/abortionFormHandler.js
+++ pregnancy/abortionFormHandler.js
@@ -9,6 +9,7 @@ import {
     WithName
 } from 'rules-config/rules';
 import lib from '../lib';
+const _ = require("lodash");
 
 const AbortionFormViewFilter = RuleFactory("6ff66edf-30c9-49c5-821c-c44f371b31b2", "ViewFilter");
 const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
@@ -20,6 +21,21 @@ class AbortionFormViewFilterHandlerJNPCT {
         return FormElementsStatusHelper
             .getFormElementsStatusesWithoutDefaults(new AbortionFormViewFilterHandlerJNPCT(), programEncounter, formElementGroup, today);
     }
+// Period of gestation in weeks at the time of Abortion/MTP
+// periodOfGestationInWeeksAtTheTimeOfAbortionMtp(programEncounter, formElement) {
+
+    @WithName('Period of gestation in weeks at the time of Abortion/MTP')
+    m5(programEncounter, formElement) {
+        const dateOfAbortion = programEncounter.getObservationValue('Date of Abortion/MTP');
+        const lmpDate = programEncounter.programEnrolment.getObservationValue('Last menstrual period');
+        let period ='';
+        period = lib.C.weeksBetween(dateOfAbortion,lmpDate);    
+        console.log('dateOfAbortion',dateOfAbortion);
+        console.log('lmpDate',lmpDate);
+        
+        return new FormElementStatus(formElement.uuid, true, _.round(period));
+    }
+
 
   @WithName("Type of Abortion")
   @WithStatusBuilder
